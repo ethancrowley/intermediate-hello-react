06c09970efb4c2082217cf53b3161c2aea81a173
Zac Collier <zacacollier@gmail.com>
2017-05-25 19:38:23 -0500

    Add project bones

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(props) {
+  return (
+    <div>
+      <h1 className="app-header">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
